9b2530e138b95c3c2922f869629e32068f76ab75
drean <souzacruz.andreluis@gmail.com>
2024-08-18 19:05:05 -0300

    criado scaner de dados

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,28 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Por favor, digite o numero da sua Conta");
+        int numero = scanner.nextInt();
+
+        System.out.println("Por favor, digite o numero da sua Agência");
+        String agencia = scanner.next();
+
+        System.out.println("Por favor, digite o numero da seu Nome");
+        String nomecliente = scanner.next();
+
+        System.out.println("Por favor, digite o numero da seu saldo");
+        double saldo = scanner.nextDouble();       
+        
+
+        
+        System.out.println("Ola " + nomecliente + " obrigado por criar uma conta emm nosso banco, sua agência é " + agencia + " conta "
+         + numero + " seu saldo " + saldo + " ja esta disponível para saque");
+        
+         System.out.println();
+     
     }
 }
